2800493893f2b8825751852d5b410b70f1dbb15e
nuree <83565990+nureeee@users.noreply.github.com>
2021-09-17 14:51:14 +0900

    Add files via upload

---
+++ chapter1.py
@@ -0,0 +1,19 @@
+import cv2
+# print('package imported')
+#
+# img = cv2.imread("C:/Users/CPB06GameN/PycharmProjects/opencv/resource/1-min.png")
+#
+# cv2.imshow("output", img)
+# cv2.waitKey(0)
+
+# 웹캠을 볼 수 있음
+# cap = cv2.VideoCapture(0) # 0 대신 동영상 링크 넣으면 그영상 보임
+# cap.set(4,480)
+# cap.set(10,100) # 밝은 색을 잘 잡아줌
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('video', img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'): # q를 누르몉 동영상 닫힘
+#         break
+
